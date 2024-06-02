56f6c4a2a484f02378588f4836c9738c6e0e906e
leonkosnar <leon@kosnar.at>
2024-06-02 19:28:38 +0200

    added main layout and popover

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -38,40 +38,6 @@ const getProducts = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-  const { priceRange, category, name } = req.query;
-
-  if (!priceRange && !category && !name) {
-    return res.status(400).json({ message: "At least one filter parameter must be provided" });
-  }
-
-  let filters = {};
-
-  if (priceRange) {
-    const [minPrice, maxPrice] = priceRange.split('-').map(Number);
-    if (isNaN(minPrice) || isNaN(maxPrice)) {
-      return res.status(400).json({ message: "Invalid priceRange format. Expected format: minPrice-maxPrice" });
-    }
-    filters.price = { $gte: minPrice, $lte: maxPrice };
-  }
-
-  if (category) {
-    const categoryExists = await Category.findOne({ _id: category }).exec();
-    if (!categoryExists) {
-      return res.status(400).json({ message: `Category with ID ${category} does not exist` });
-    }
-    filters.category = category;
-  }
-
-  if (name) {
-    filters.name = { $regex: name, $options: 'i' }; // Case insensitive regex search
-  }
-
-  try {
-    const data = await Product.find(filters);
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 //* GET PRODUCTS BY CATEGORY
@@ -104,26 +70,6 @@ const createProduct = async (req, res) => {
   try {
     const { name, description, price, stock, images, categoryID } = req.body;
 
-    // Check if required fields are present
-    if (!name) {
-      return res.status(400).json({ message: "Product name is required" });
-    }
-    if (!price) {
-      return res.status(400).json({ message: "Product price is required" });
-    }
-    if (!images || images.length === 0) {
-      return res.status(400).json({ message: "At least one image is required" });
-    }
-    if (!categoryID) {
-      return res.status(400).json({ message: "categoryID is required" });
-    }
-
-    // Check if the category exists
-    const categoryExists = await Category.findOne({ _id: categoryID }).exec();
-    if (!categoryExists) {
-      return res.status(400).json({ message: `Category with ID ${categoryID} does not exist` });
-    }
-
     // Check if a product with the given name already exists
     // Check if required fields are present
     if (!name) {
@@ -148,10 +94,6 @@ const createProduct = async (req, res) => {
     // Check if a product with the given name already exists
     const product = await Product.findOne({ name }).exec();
     if (product) {
-      return res.status(400).json({ message: `A Product with the given name: ${name} already exists` });
-    }
-
-    // Create new product
       return res.status(400).json({ message: `A Product with the given name: ${name} already exists` });
     }
 
